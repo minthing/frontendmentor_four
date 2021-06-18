682f493c11327dc02e9c6ed903de31ea3e1c8c35
minthing <minjeongandk@gmail.com>
2021-06-18 09:23:27 +0900

    safari에서도 움직이도록 로직 수정 + 접근성 이슈 해결

--- README.md
+++ README.md
@@ -8,10 +8,41 @@ Thanks for checking out this front-end coding challenge.
 
 [Frontend Mentor](https://www.frontendmentor.io) challenges help you improve your coding skills by building realistic projects.
 
-완성본 : https://compassionate-booth-f5db51.netlify.app/
+📍 완성본 : https://compassionate-booth-f5db51.netlify.app/
 
 ##### 발견한 문제점
 
 * label element, title undefined, aria-label undefined, aria-labelledby undefined. -> 접근성에 관련해서 제출하기 전에 미리 해당 부분에 대해 알아봤어야 했다. input에 해당 접근성에 관한 요소가 들어간다는 사실을 알지 못했음. 이후 조사해 볼 것.
 
 * 크롬 개발자 도구로 보았을 때는 정상적으로 화면이 출력되었는데, 실제 모바일 기기에서 보니 배경과 타이틀이 깨져서 나오는대다 range가 움직이지 않는 이슈가 있음. 추후 수정할 것.
+
+* 사파리에서 range input 작동 안함. 고질적인 문제인듯. 아예 range를 사용하지 말아야 하나 고민중..
+(스텍오버플로우에서 가져온 js 수정법 : https://stackoverflow.com/questions/33343854/input-range-slider-not-working-on-ios-safari-when-clicking-on-track)
+```javascript
+
+let diagramSlider = document.querySelector(".slider");
+
+function iosPolyfill(e) {
+  let val = (e.pageX - diagramSlider.getBoundingClientRect().left) /
+   (diagramSlider.getBoundingClientRect().right - diagramSlider.getBoundingClientRect().left),
+  max = diagramSlider.getAttribute("max"),
+  segment = 1 / (max - 1),
+  segmentArr = [];
+
+  max++;
+
+  for (let i = 0; i < max; i++) {
+    segmentArr.push(segment * i);
+  }
+
+  let segCopy = segmentArr.slice(),
+  ind = segmentArr.sort((a, b) => Math.abs(val - a) - Math.abs(val - b))[0];
+
+  diagramSlider.value = segCopy.indexOf(ind) + 1;
+}
+
+if (!!navigator.platform.match(/iPhone|iPod|iPad/)) {
+  diagramSlider.addEventListener("touchend", iosPolyfill, {passive: true});
+}
+
+```
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const pageview_array = ["10K", "50K", "100K", "500K", "1M"];
-let price_array = ["$8.00", "$12.00", "$16.00", "$24.00", "$32.00"];
+const pageview_array = ["10K", "50K", "100K", "500K", "1M", "1M"];
+let price_array = ["$8.00", "$12.00", "$16.00", "$24.00", "$32.00", "$32.00"];
 
 const slider = document.querySelector('.slider');
 const box = document.querySelector('.box');
@@ -9,26 +9,52 @@ const pageview_text = box.querySelector('.left em');
 
 let percentage = 2;
 
-console.log(price_text, pageview_text)
-
 checkbox.addEventListener('click', e => {
   if(checkbox.checked == true){
-    price_array = ["$72.00", "$108.00", "$144.00", "$216.00", "$324.00"]
+    price_array = ["$72.00", "$108.00", "$144.00", "$216.00", "$324.00", "$324.00"]
     price_text.innerHTML = price_array[percentage];
   }else{
-    price_array = ["$8.00", "$12.00", "$16.00", "$24.00", "$32.00"];
+    price_array = ["$8.00", "$12.00", "$16.00", "$24.00", "$32.00", "$32.00"];
     price_text.innerHTML = price_array[percentage];
   }
 })
 
 slider.addEventListener('change', e => {
-  let value = e.target.value;
-  percentage = value-1;
-  let green = (percentage) * 25
+  let value = Math.floor(e.target.value*0.05);
+  percentage = value;
+//   if(percentage != 0){
+//       percentage = value-1
+//   }
+  let green = e.target.value;
+  console.log(percentage)
     slider.style.background = "linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) "
   + green + "%, hsl(224, 65%, 95%) " + green + "%, hsl(224, 65%, 95%) 100%)";
 
-  console.log(price_array[percentage]);
   price_text.innerHTML = price_array[percentage];
   pageview_text.innerHTML = pageview_array[percentage];
 })
+
+let diagramSlider = document.querySelector(".slider");
+
+function iosPolyfill(e) {
+  let val = (e.pageX - diagramSlider.getBoundingClientRect().left) /
+   (diagramSlider.getBoundingClientRect().right - diagramSlider.getBoundingClientRect().left),
+  max = diagramSlider.getAttribute("max"),
+  segment = 1 / (max - 1),
+  segmentArr = [];
+
+  max++;
+
+  for (let i = 0; i < max; i++) {
+    segmentArr.push(segment * i);
+  }
+
+  let segCopy = segmentArr.slice(),
+  ind = segmentArr.sort((a, b) => Math.abs(val - a) - Math.abs(val - b))[0];
+
+  diagramSlider.value = segCopy.indexOf(ind) + 1;
+}
+
+if (!!navigator.platform.match(/iPhone|iPod|iPad/)) {
+  diagramSlider.addEventListener("touchend", iosPolyfill, {passive: true});
+}
